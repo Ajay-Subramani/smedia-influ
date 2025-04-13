94ac86f0fe2039840e89df305a27b83c8d541ab3
Ajay Subramani <145855051+Ajay-Subramani@users.noreply.github.com>
2025-04-13 21:45:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb+srv://ajaysubramani:mongodb%40ajay%40123@cluster0.f4yxo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+mongoose.connect("mongodb+srv://ajaysubramani%40gmail.com:mongodb%40ajay%40123@cluster0.f4yxo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
 const formSchema = new mongoose.Schema({
   fname: String,
